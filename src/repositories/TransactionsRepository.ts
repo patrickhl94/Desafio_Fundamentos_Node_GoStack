0a144eac05733e46325254b5ed210417baacff37
Patrick Lima <patrickhl94@hotmail.com>
2020-04-24 00:24:01 -0300

    Updated TransactionsRepository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -19,15 +19,27 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    this.getBalance();
-    return this.transactions;
+  public all(): object {
+    return { transactions: this.transactions, balance: this.getBalance() };
   }
 
   public getBalance(): Balance {
-    console.log(
-      this.transactions.filter(transaction => transaction.type === 'income'),
-    );
+    const income = this.transactions
+      .filter(transaction => transaction.type === 'income')
+      .reduce(
+        (accumulator, currentValue) => accumulator + currentValue.value,
+        0,
+      );
+    const outcome = this.transactions
+      .filter(transaction => transaction.type === 'outcome')
+      .reduce(
+        (accumulator, currentValue) => accumulator + currentValue.value,
+        0,
+      );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
   public create({ type, value, title }: RequestDTO): Transaction {
