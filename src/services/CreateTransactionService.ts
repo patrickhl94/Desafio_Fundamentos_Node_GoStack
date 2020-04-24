6b3abb79eba15aef9e8e82dcc177c08a5881fc81
Patrick Lima <patrickhl94@hotmail.com>
2020-04-24 00:24:16 -0300

    Updated CreateTransactionsService

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -19,6 +19,12 @@ class CreateTransactionService {
       throw Error('The type entered is not allowed');
     }
 
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (type === 'outcome' && value > total) {
+      throw Error('The value is less than the total available');
+    }
+
     const transaction = this.transactionsRepository.create({
       title,
       value,
